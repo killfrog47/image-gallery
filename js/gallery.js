023246ade60ba6aec4b22cc6182a3cf77ffa556f
Tomas Cordero <tcorderoiv@gmail.com>
2014-10-01 23:18:00 -0700

    Added some basic image loading
    Made thumbnails work when clicked on and added algorithm to calculate
    the width of the thumbnail list

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,16 @@
 $(function(){
-	var featureImage = $('#feauredImg');
+	var featureImage = $('#featuredImg');
 
-	console.log($('.galleryThumb').data("bigimg"));
+	$('.thumb').click(function(){
+		var indexNum = $(this).index();
+		var newUrl = $(this).data('image');
+		$(featureImage).attr('src', newUrl);
+		$('.thumb img.active').removeClass('active');
+		$("li img.galleryThumb").eq(indexNum).addClass('active');
+	})
+	
+	var numOfThumbs = $("#galleryThumbs li").length;
+	
+	var thumbListLength = (100*numOfThumbs)+(5*(numOfThumbs))-645;
+	console.log(thumbListLength);
 })
